be825ebad68d410cb28cd9d85a07e776e476dbf0
Avinash <avinash31803@gmail.com>
2024-03-11 10:58:34 +0530

    update : ui of user modal

--- frontend/src/components/chat/ProfileModel.js
+++ frontend/src/components/chat/ProfileModel.js
@@ -1,11 +1,11 @@
-import { ModalOverlay, useDisclosure, ModalBody, Button, Modal, ModalContent, ModalHeader, ModalCloseButton, Text, ModalFooter } from '@chakra-ui/react'
+import { ModalOverlay, useDisclosure, ModalBody, Button, Modal, ModalContent, ModalHeader, ModalCloseButton, Text, ModalFooter, Card, CardBody, Image, Stack, Heading, Divider, CardFooter, ButtonGroup } from '@chakra-ui/react'
 import React, { useState } from 'react';
 
-const ProfileModel = ({user,children}) => {
+const ProfileModel = ({ user, children }) => {
     const OverlayOne = () => (
         <ModalOverlay
             bg='blackAlpha.300'
-            backdropFilter='blur(10px) hue-rotate(90deg)'
+            backdropFilter='blur(10px)'
         />
     )
 
@@ -13,7 +13,7 @@ const ProfileModel = ({user,children}) => {
     const [overlay, setOverlay] = React.useState(<OverlayOne />)
     return (
         <>
-            <Button 
+            <Button
                 width='100%'
                 backgroundColor='white'
                 onClick={() => {
@@ -26,14 +26,33 @@ const ProfileModel = ({user,children}) => {
             <Modal isCentered isOpen={isOpen} onClose={onClose}>
                 {overlay}
                 <ModalContent>
-                    <ModalHeader>{user.name}</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Text>Custom backdrop filters!</Text>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button onClick={onClose}>Close</Button>
-                    </ModalFooter>
+                    <Card width='100%'>
+                        <ModalCloseButton margin='-0.75rem -1rem' />
+                        <CardBody>
+                            <Image
+                                src={user.pic}
+                                alt='{user.name}'
+                                borderRadius='lg'
+                            />
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md'>Name : {user.name}</Heading>
+                                <Text>
+                                    <b>Email : </b>{user.email}
+                                </Text>
+                            </Stack>
+                        </CardBody>
+                        <Divider />
+                        <CardFooter>
+                            <ButtonGroup spacing='2' width='100%' justifyContent='end'>
+                                <Button variant='solid' colorScheme='red' onClick={onClose}>
+                                 Close
+                                </Button>
+                                {/* <Button variant='ghost' colorScheme='blue'>
+                                    Add to cart
+                                </Button> */}
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
                 </ModalContent>
             </Modal>
         </>
